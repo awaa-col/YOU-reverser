9b8b18351d7701a445bc789c08876dfe20fb3b92
awaa-col <23964939@qq.com>
2025-03-07 13:24:43 +0000

    添加必要的库

--- 安装依赖.bat
+++ 安装依赖.bat
@@ -1,5 +1,2 @@
 python -m venv venv
 start cmd /k "venv\Scripts\activate && python -m pip install -r requirements.txt && pause && exit"
-git rm --cached 安装依赖.bat
-git add start.bat
-git commit -m "修复 start.bat 乱码问题"
